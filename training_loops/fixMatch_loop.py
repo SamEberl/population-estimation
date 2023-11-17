a49ff023f4f0185537d677fd6cfc3d67102a4e1a
SamEberl <sam.eberl@tutanota.com>
2023-11-17 14:28:50 +0100

    log some images to see noise lvl

--- training_loops/fixMatch_loop.py
+++ training_loops/fixMatch_loop.py
@@ -114,11 +114,14 @@ def forward_pass(student_model,
     writer.add_scalar(f'Loss-Total/{split}', loss, step_nbr)
     # print(f'Loss-total: {loss}')
 
-    # if save_img:
-    #     #sample_nbr = random.randint(0, len(student_inputs[:, 0, 0, 0]-1))
-    #     writer.add_images(f'Panel_Images', student_inputs[0, :3, :, :], global_step=step_nbr, dataformats='CHW')
-    #     log_regression_plot_to_tensorboard(writer, step_nbr, labels.cpu().flatten(),
-    #                                        student_preds.cpu().flatten())
+    save_img = True
+    if save_img:
+        #sample_nbr = random.randint(0, len(student_inputs[:, 0, 0, 0]-1))
+        writer.add_images(f'Panel_Images', student_inputs[0, :3, :, :], global_step=step_nbr, dataformats='CHW')
+        log_regression_plot_to_tensorboard(writer, step_nbr, labels.cpu().flatten(),
+                                           student_preds.cpu().flatten())
+        if random.randint(0, 10) < 2:
+            exit()
 
     return loss
 
