fix contrastive loss computation

training_loops/fixMatch_loop.py
@@ -103,14 +103,15 @@ def forward_pass(student_model,
         #print(f'Percentage-used-unsupervised: {torch.sum(pseudo_label_mask)/len(pseudo_label_mask)}')
         # pseudo_label_mask = teacher_data_uncertainty < ?
         if torch.sum(pseudo_label_mask) > 0:
+            batch_size = student_features.size(0)
             # positive examples
-            Y = torch.ones(config['data_params']['train_batch_size'], dtype=torch.float32)
+            Y = torch.ones(batch_size, dtype=torch.float32)
             if torch.cuda.is_available():
                 Y = Y.cuda()
             positive_loss = student_model.loss_unsupervised(student_features, teacher_features_mean, pseudo_label_mask, Y)
             # negative examples
             dearanged_teacher_features = derangement_shuffle(teacher_features)
-            Y = torch.zeros(config['data_params']['train_batch_size'], dtype=torch.float32)
+            Y = torch.zeros(batch_size, dtype=torch.float32)
             if torch.cuda.is_available():
                 Y = Y.cuda()
             negative_loss = student_model.loss_unsupervised(student_features, dearanged_teacher_features, pseudo_label_mask, Y)