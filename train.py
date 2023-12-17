f607d1fa499146ff35b3d82b5316403a730a7c79
SamEberl <sam.eberl@tutanota.com>
2023-12-17 15:41:54 +0100

    remove hparam search

--- train.py
+++ train.py
@@ -44,42 +44,26 @@ writer.add_text('Model Specs', model_yaml_str, 0)
 student_transform, teacher_transform = get_transforms(config)
 train_dataloader, val_dataloader = get_dataloader(config, student_transform, teacher_transform)
 
-if config['hparam_search']['active']:
-    hparam_name = config['hparam_search']['hparam_name']
-    n = config['hparam_search']['nbr_values']
-    lowest = (config['hparam_search']['lowest'])
-    highest = (config['hparam_search']['highest'])
-    param_list = np.geomspace(lowest, highest, n)
-    decimal_places = count_decimal_places(lowest) + 1
-    rounded_param_list = [round(value, decimal_places) for value in param_list]
-    rounded_param_list = [5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05,
-                          5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05, 5e-05]
-    print(f'param_list: {rounded_param_list}')
-    for i in range(n):
-        student_model_temp = copy.deepcopy(student_model)
-        teacher_model_temp = copy.deepcopy(teacher_model)
-        config['train_params'][hparam_name] = rounded_param_list[i]
-        train_fix_match(config, writer, student_model_temp, teacher_model_temp, train_dataloader, val_dataloader)
-else:
-    train_loss, val_loss = train_fix_match(config, writer, student_model, teacher_model, train_dataloader, val_dataloader)
 
-    save_path = os.path.join(config['save_dirs']['model_save_dir'],
-                             f"{config['model_params']['pretrained_weights']}_{current_datetime}.pt")
-    print(f'Saving model under {save_path}')
-    torch.save(student_model.state_dict(), save_path)
+train_loss, val_loss = train_fix_match(config, writer, student_model, teacher_model, train_dataloader, val_dataloader)
 
-    # writer.add_hparams({
-    #     'in_channels': config['model_params']['in_channels'],
-    #     'retrain': config['model_params']['retrain'],
-    #     'drop_rate': config['model_params']['drop_rate'],
-    #     'train_batch_size': config['data_params']['train_batch_size'],
-    #     'LR': config['train_params']['LR'],
-    #     'L2_reg': config['train_params']['L2_reg'],
-    #     'beta1': config['train_params']['beta1'],
-    #     'beta2': config['train_params']['beta2'],
-    #     'ema_alpha': config['train_params']['ema_alpha']},
-    #     {'train_loss': train_loss,
-    #      'val_loss': val_loss}, name='')
+save_path = os.path.join(config['save_dirs']['model_save_dir'],
+                         f"{config['model_params']['pretrained_weights']}_{current_datetime}.pt")
+print(f'Saving model under {save_path}')
+torch.save(student_model.state_dict(), save_path)
+
+# writer.add_hparams({
+#     'in_channels': config['model_params']['in_channels'],
+#     'retrain': config['model_params']['retrain'],
+#     'drop_rate': config['model_params']['drop_rate'],
+#     'train_batch_size': config['data_params']['train_batch_size'],
+#     'LR': config['train_params']['LR'],
+#     'L2_reg': config['train_params']['L2_reg'],
+#     'beta1': config['train_params']['beta1'],
+#     'beta2': config['train_params']['beta2'],
+#     'ema_alpha': config['train_params']['ema_alpha']},
+#     {'train_loss': train_loss,
+#      'val_loss': val_loss}, name='')
 
 # Close the SummaryWriter after training
 writer.close()
