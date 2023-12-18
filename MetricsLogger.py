774922dcd27ebe13d0ad16ccb482e5880fc9efae
SamEberl <sam.eberl@tutanota.com>
2023-12-18 18:58:37 +0100

    fix logger

--- MetricsLogger.py
+++ MetricsLogger.py
@@ -78,8 +78,8 @@ class MetricsLogger:
     def write(self, step_nbr):
         """Write all accumulated metrics to TensorBoard."""
         for metric_name, values in self.metrics.items():
-            # print(f'metric_name: {metric_name}')
-            # print(f'values: {values}')
+            print(f'metric_name: {metric_name}')
+            print(f'values: {values}')
             if 'Observe-Bias' in metric_name:
                 if 'train' in metric_name:
                     calc_bias(values)
