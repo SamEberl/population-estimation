da4c019c8faad7dc522da74d1fca70ed02fc0762
SamEberl <sam.eberl@tutanota.com>
2023-12-18 09:46:09 +0100

    change hparams

--- plot_stuff.py
+++ plot_stuff.py
@@ -218,7 +218,13 @@ def visualize_features(features, labels=None, method='pca', save_path='feature_v
     return save_path
 
 # Example usage:
-# visualize_features(your_feature_array, labels=your_labels, method='pca', save_path='/mnt/data/pca_visualization.png')
+import pandas as pd
+# features = pd.read_csv('/home/sam/Desktop/train_features_full.csv')
+features = pd.read_csv('/home/sam/Desktop/val_features_sen2spring_full.csv')
+print(features.shape)
+features_val = features.iloc[:, :-1]
+labels = features.iloc[:, -1]
+visualize_features(features, labels=labels, method='tsne', save_path='/home/sam/Desktop/so2sat_test/tsne_val_visualization.png')
 # Note: Replace 'your_feature_array' and 'your_labels' with your actual data.
 
 
