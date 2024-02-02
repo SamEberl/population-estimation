b74b969581b2e011b26f5d9e0b7875d6c70bd470
SamEberl <sam.eberl@tutanota.com>
2024-02-02 16:11:16 +0100

    fix

--- MetricsLogger.py
+++ MetricsLogger.py
@@ -89,8 +89,8 @@ class MetricsLogger:
         uncertainty = uncertainty.to('cpu').detach().numpy()
         if uncertainty_name not in self.uncertainties:
             self.uncertainties[uncertainty_name] = np.array([])
-        print(f'\nname: {uncertainty_name}')
-        print(f'new: {uncertainty.shape}  |  old: {self.uncertainties[uncertainty_name].shape}')
+        # print(f'\nname: {uncertainty_name}')
+        # print(f'new: {uncertainty.shape}  |  old: {self.uncertainties[uncertainty_name].shape}')
         np.concatenate((self.uncertainties[uncertainty_name], uncertainty))
 
     def save_uncertainties(self):
