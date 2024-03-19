c90812832ff01f68d87d1270d6635d8066c56e8d
SamEberl <sam.eberl@tutanota.com>
2024-03-19 18:39:08 +0100

    automate training

---
+++ train_multiple.py
@@ -0,0 +1,16 @@
+import subprocess
+
+# Define the different file paths in a list
+file_paths = [
+    "fixMatch_l2_0p1",
+    "fixMatch_l2_1",
+    "fixMatch_drop_1",
+    "fixMatch_drop_2",
+    "fixMatch_drop_3",
+    "fixMatch_drop_4",
+    "fixMatch_drop_5",
+]
+
+# Loop through the file paths and call train.py with each one
+for path in file_paths:
+    subprocess.run(["python", "train.py", "--file_path", path])
