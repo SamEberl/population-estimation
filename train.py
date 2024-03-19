5c1fd397405e8c19a879a99b86cd12c8c8b50ece
SamEberl <sam.eberl@tutanota.com>
2024-03-19 18:48:04 +0100

    automate training

--- train.py
+++ train.py
@@ -7,81 +7,70 @@ from logging_utils import *
 from models import *
 from utils import parse_yaml
 
-def main(file_path):
-    # Your existing code that uses the file_path
-    print(f"--- Training on file: {file_path} ---")
-    config = parse_yaml(f'configs/fixMatch_l2_0p1.yaml')
-    #config = parse_yaml('configs/fixMatch_local.yaml')
 
-    seed = config['train_params']['seed']
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(seed)
+parser = argparse.ArgumentParser(description='Train a model with a specified file path.')
+parser.add_argument('--file_path', type=str, required=True, help='The path to the file to train on.')
+args = parser.parse_args()
+file_path = args.file_path
+print(f"--- Training on file: {file_path} ---")
+config = parse_yaml(f'configs/{file_path}.yaml')
+#config = parse_yaml('configs/fixMatch_local.yaml')
 
-    # Choose device
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+seed = config['train_params']['seed']
+np.random.seed(seed)
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
 
-    # Init models
-    student_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
-    teacher_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
+# Choose device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Retrain from checkpoint
-    if config['model_params']['retrain']:
-        retrain_from = config['model_params']['retrain_from']
-        student_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
-        teacher_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
+# Init models
+student_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
+teacher_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
 
-    # Set logging directory
-    log_dir = config['save_dirs']['log_save_dir']
-    current_datetime = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
-    print(f'--- Start training at {current_datetime} ---')
+# Retrain from checkpoint
+if config['model_params']['retrain']:
+    retrain_from = config['model_params']['retrain_from']
+    student_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
+    teacher_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
 
-    # Create a SummaryWriter for TensorBoard
-    writer = SummaryWriter(logdir=log_dir + config['model_params']['architecture'] + '-' + current_datetime)
+# Set logging directory
+log_dir = config['save_dirs']['log_save_dir']
+current_datetime = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+print(f'--- Start training at {current_datetime} ---')
 
-    train_dataloader, valid_dataloader, train_dataloader_unlabeled = get_dataloaders(config)
+# Create a SummaryWriter for TensorBoard
+writer = SummaryWriter(logdir=log_dir + config['model_params']['architecture'] + '-' + current_datetime)
 
-    train_fix_match(config,
-                    writer,
-                    student_model,
-                    teacher_model,
-                    train_dataloader,
-                    valid_dataloader,
-                    train_dataloader_unlabeled)
+train_dataloader, valid_dataloader, train_dataloader_unlabeled = get_dataloaders(config)
 
-    save_path = os.path.join(config['save_dirs']['model_save_dir'],
-                             f"{config['model_params']['pretrained_weights']}_{current_datetime}.pt")
-    student_model.eval()
-    print(f'Saving model under {save_path}')
-    torch.save(student_model.state_dict(), save_path)
+train_fix_match(config,
+                writer,
+                student_model,
+                teacher_model,
+                train_dataloader,
+                valid_dataloader,
+                train_dataloader_unlabeled)
 
-    # writer.add_hparams({
-    #     'in_channels': config['model_params']['in_channels'],
-    #     'retrain': config['model_params']['retrain'],
-    #     'drop_rate': config['model_params']['drop_rate'],
-    #     'train_batch_size': config['data_params']['train_batch_size'],
-    #     'LR': config['train_params']['LR'],
-    #     'L2_reg': config['train_params']['L2_reg'],
-    #     'beta1': config['train_params']['beta1'],
-    #     'beta2': config['train_params']['beta2'],
-    #     'ema_alpha': config['train_params']['ema_alpha']},
-    #     {'train_loss': train_loss,
-    #      'val_loss': val_loss}, name='')
+save_path = os.path.join(config['save_dirs']['model_save_dir'],
+                         f"{config['model_params']['pretrained_weights']}_{current_datetime}.pt")
+student_model.eval()
+print(f'Saving model under {save_path}')
+torch.save(student_model.state_dict(), save_path)
 
-    # Close the SummaryWriter after training
-    writer.close()
+# writer.add_hparams({
+#     'in_channels': config['model_params']['in_channels'],
+#     'retrain': config['model_params']['retrain'],
+#     'drop_rate': config['model_params']['drop_rate'],
+#     'train_batch_size': config['data_params']['train_batch_size'],
+#     'LR': config['train_params']['LR'],
+#     'L2_reg': config['train_params']['L2_reg'],
+#     'beta1': config['train_params']['beta1'],
+#     'beta2': config['train_params']['beta2'],
+#     'ema_alpha': config['train_params']['ema_alpha']},
+#     {'train_loss': train_loss,
+#      'val_loss': val_loss}, name='')
 
-
-if __name__ == "__main__":
-    # Create the parser
-    parser = argparse.ArgumentParser(description='Train a model with a specified file path.')
-
-    # Add an argument for the file path
-    parser.add_argument('--file_path', type=str, required=True, help='The path to the file to train on.')
-
-    # Parse the arguments
-    args = parser.parse_args()
-
-    # Call the main function with the file path
-    main(args.file_path)
+# Close the SummaryWriter after training
+writer.close()
