4e14d1cedddef008f096245f063d67d3e141bb9c
SamEberl <sam.eberl@tutanota.com>
2024-03-22 10:39:24 +0100

    load configs immediately

--- train.py
+++ train.py
@@ -8,69 +8,62 @@ from models import *
 from utils import parse_yaml
 
 
-parser = argparse.ArgumentParser(description='Train a model with a specified file path.')
-parser.add_argument('--file_path', type=str, required=True, help='The path to the file to train on.')
-args = parser.parse_args()
-file_path = args.file_path
-print(f"--- Training on file: {file_path} ---")
-config = parse_yaml(file_path)
-#config = parse_yaml('configs/fixMatch_local.yaml')
+def train_main(config):
+    seed = config['train_params']['seed']
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
-seed = config['train_params']['seed']
-np.random.seed(seed)
-torch.manual_seed(seed)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(seed)
+    # Choose device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Choose device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Init models
+    student_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
+    teacher_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
 
-# Init models
-student_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
-teacher_model = ssl_models[config['model_params']['architecture']](**config['model_params']).to(device)
+    # Retrain from checkpoint
+    if config['model_params']['retrain']:
+        retrain_from = config['model_params']['retrain_from']
+        student_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
+        teacher_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
 
-# Retrain from checkpoint
-if config['model_params']['retrain']:
-    retrain_from = config['model_params']['retrain_from']
-    student_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
-    teacher_model.load_state_dict(torch.load(os.path.join(config['save_dirs']['model_save_dir'], retrain_from)))
+    # Set logging directory
+    log_dir = config['save_dirs']['log_save_dir']
+    current_datetime = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+    print(f'--- Start training at {current_datetime} ---')
 
-# Set logging directory
-log_dir = config['save_dirs']['log_save_dir']
-current_datetime = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
-print(f'--- Start training at {current_datetime} ---')
+    # Create a SummaryWriter for TensorBoard
+    writer = SummaryWriter(logdir=log_dir + config['model_params']['architecture'] + '-' + current_datetime)
 
-# Create a SummaryWriter for TensorBoard
-writer = SummaryWriter(logdir=log_dir + config['model_params']['architecture'] + '-' + current_datetime)
+    train_dataloader, valid_dataloader, train_dataloader_unlabeled = get_dataloaders(config)
 
-train_dataloader, valid_dataloader, train_dataloader_unlabeled = get_dataloaders(config)
+    train_fix_match(config,
+                    writer,
+                    student_model,
+                    teacher_model,
+                    train_dataloader,
+                    valid_dataloader,
+                    train_dataloader_unlabeled)
 
-train_fix_match(config,
-                writer,
-                student_model,
-                teacher_model,
-                train_dataloader,
-                valid_dataloader,
-                train_dataloader_unlabeled)
+    save_path = os.path.join(config['save_dirs']['model_save_dir'],
+                             f"{config['model_params']['pretrained_weights']}_{current_datetime}.pt")
+    student_model.eval()
+    print(f'Saving model under {save_path}')
+    torch.save(student_model.state_dict(), save_path)
 
-save_path = os.path.join(config['save_dirs']['model_save_dir'],
-                         f"{config['model_params']['pretrained_weights']}_{current_datetime}.pt")
-student_model.eval()
-print(f'Saving model under {save_path}')
-torch.save(student_model.state_dict(), save_path)
+    # writer.add_hparams({
+    #     'in_channels': config['model_params']['in_channels'],
+    #     'retrain': config['model_params']['retrain'],
+    #     'drop_rate': config['model_params']['drop_rate'],
+    #     'train_batch_size': config['data_params']['train_batch_size'],
+    #     'LR': config['train_params']['LR'],
+    #     'L2_reg': config['train_params']['L2_reg'],
+    #     'beta1': config['train_params']['beta1'],
+    #     'beta2': config['train_params']['beta2'],
+    #     'ema_alpha': config['train_params']['ema_alpha']},
+    #     {'train_loss': train_loss,
+    #      'val_loss': val_loss}, name='')
 
-# writer.add_hparams({
-#     'in_channels': config['model_params']['in_channels'],
-#     'retrain': config['model_params']['retrain'],
-#     'drop_rate': config['model_params']['drop_rate'],
-#     'train_batch_size': config['data_params']['train_batch_size'],
-#     'LR': config['train_params']['LR'],
-#     'L2_reg': config['train_params']['L2_reg'],
-#     'beta1': config['train_params']['beta1'],
-#     'beta2': config['train_params']['beta2'],
-#     'ema_alpha': config['train_params']['ema_alpha']},
-#     {'train_loss': train_loss,
-#      'val_loss': val_loss}, name='')
-
-# Close the SummaryWriter after training
-writer.close()
+    # Close the SummaryWriter after training
+    writer.close()
--- train_multiple.py
+++ train_multiple.py
@@ -1,4 +1,6 @@
-import subprocess
+from utils import parse_yaml
+from train import train_main
+
 
 # Define the different file paths in a list
 file_paths = [
@@ -9,6 +11,10 @@ file_paths = [
     "fixMatch_5",
 ]
 
-# Loop through the file paths and call train.py with each one
-for path in file_paths:
-    subprocess.run(["python", "train.py", "--file_path", f'configs/{path}.yaml'])
+configs = {}
+
+for i, path in enumerate(file_paths):
+    configs[i] = parse_yaml(path)
+
+for config in configs.values():
+    train_main(config)
